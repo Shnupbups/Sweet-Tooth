ed0b0b9284bd791b86d71d91846b6f48b9dbbd47
Shnupbups <shnupbups@gmail.com>
2019-08-30 22:50:57 +1000

    Added more stuff
    Added sugar dusted coal and charcoal: by crafting coal or charcoal with 2 sugar, it increases their burn time by 20 seconds.
    Also added tags for cakes, milk buckets, and dusted coals.

--- src/main/java/com/shnupbups/sweettooth/SweetTooth.java
+++ src/main/java/com/shnupbups/sweettooth/SweetTooth.java
@@ -4,7 +4,10 @@ import com.shnupbups.sweettooth.init.ModBlocks;
 import com.shnupbups.sweettooth.init.ModItems;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.impl.registry.FuelRegistryImpl;
 
+import net.minecraft.item.Item;
+import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 
 public class SweetTooth implements ModInitializer {
@@ -12,6 +15,7 @@ public class SweetTooth implements ModInitializer {
 	public void onInitialize() {
 		ModItems.init();
 		ModBlocks.init();
+		FuelRegistryImpl.INSTANCE.add(new Tag<>(getId("sugar_dusted_coals")), 2000);
 	}
 	
 	public static Identifier getId(String name) {
--- src/main/java/com/shnupbups/sweettooth/init/ModItems.java
+++ src/main/java/com/shnupbups/sweettooth/init/ModItems.java
@@ -16,6 +16,8 @@ public class ModItems {
 	public static ChocolateMilkBucketItem CHOCOLATE_MILK_BUCKET = new ChocolateMilkBucketItem(new Item.Settings().recipeRemainder(Items.BUCKET).group(ItemGroup.MISC).maxCount(1));
 	public static Item SWEET_JAM = new Item(new Item.Settings().recipeRemainder(Items.GLASS_BOTTLE).group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(6).saturationModifier(9.6f).build()));
 	public static FoodOnAStickItem CARAMEL_APPLE = new FoodOnAStickItem(new Item.Settings().recipeRemainder(Items.STICK).group(ItemGroup.FOOD).maxCount(1).food(new FoodComponent.Builder().hunger(4).saturationModifier(0.4f).build()));
+	public static Item SUGAR_DUSTED_COAL = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+	public static Item SUGAR_DUSTED_CHARCOAL = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
 	
 	public static void init() {
 		register("cotton_candy", COTTON_CANDY);
@@ -24,6 +26,8 @@ public class ModItems {
 		register("chocolate_milk_bucket", CHOCOLATE_MILK_BUCKET);
 		register("sweet_jam", SWEET_JAM);
 		register("caramel_apple", CARAMEL_APPLE);
+		register("sugar_dusted_coal", SUGAR_DUSTED_COAL);
+		register("sugar_dusted_charcoal", SUGAR_DUSTED_CHARCOAL);
 	}
 	
 	public static void register(String name, Item item) {
