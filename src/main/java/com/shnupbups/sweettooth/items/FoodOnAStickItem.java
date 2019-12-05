fe3fc3e0c426ae223a920125554c4f9249052c70
Yoghurt4C <yoghurt4242@gmail.com>
2019-12-05 14:45:57 +0300

    Update .gitignore

--- src/main/java/com/shnupbups/sweettooth/items/FoodOnAStickItem.java
+++ src/main/java/com/shnupbups/sweettooth/items/FoodOnAStickItem.java
@@ -1,6 +1,7 @@
 package com.shnupbups.sweettooth.items;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -14,6 +15,18 @@ public class FoodOnAStickItem extends Item {
 	@Override
 	public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
 		super.finishUsing(stack, world, user);
-			return new ItemStack(Items.STICK);
+		PlayerEntity playerEntity = user instanceof PlayerEntity ? (PlayerEntity)user : null;
+
+		if (playerEntity == null || !playerEntity.abilities.creativeMode) {
+			if (stack.isEmpty()) {
+				return new ItemStack(Items.STICK);
+			}
+
+			if (playerEntity != null) {
+				playerEntity.inventory.insertStack(new ItemStack(Items.STICK));
+			}
+		}
+
+		return stack;
 	}
 }
