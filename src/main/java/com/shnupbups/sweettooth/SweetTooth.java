f9a5f917ba2e06538c612874ddbea98f5e07a13f
Chase Whitfield <fatlard1993@gmail.com>
2019-12-27 13:42:42 -0800

    Fix coal fuel registration

--- src/main/java/com/shnupbups/sweettooth/SweetTooth.java
+++ src/main/java/com/shnupbups/sweettooth/SweetTooth.java
@@ -1,9 +1,6 @@
 package com.shnupbups.sweettooth;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.impl.content.registry.FuelRegistryImpl;
-
-import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 
 import com.shnupbups.sweettooth.init.ModBlocks;
@@ -14,7 +11,6 @@ public class SweetTooth implements ModInitializer {
 	public void onInitialize() {
 		ModItems.init();
 		ModBlocks.init();
-		FuelRegistryImpl.INSTANCE.add(new Tag<>(getId("sugar_dusted_coals")), 2000);
 	}
 	
 	public static Identifier getId(String name) {
